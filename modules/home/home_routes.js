3513cb25f9253fd915bbae429400e0b77873458e
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-14 14:21:56 -0500

    isolate middlewarees

--- modules/home/home_routes.js
+++ modules/home/home_routes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const home_routes = express.Router(); // miniMain isolated route
 const route = "/home";
 
+home_routes.use((req, res, next) => {
+  // this middleware will affect home_routes and recipes_routes
+  // beacuse is the next route is affecting all routes
+  // I wonder if its is possible to isolate home_routes and recipes_routes
+  // in a way this middleware will affect only home_routes
+  console.log("Home route middleware");
+  next();
+})
+
 home_routes.get(route, (req, res) => {
   res.status(200)
   .send("GET Home");
