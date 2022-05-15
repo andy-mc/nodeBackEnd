95f9dd0004a56326cf8694d0296bbf6b0dc5e976
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-14 21:07:06 -0500

    use bodyParser to assign on req.body the request body to express

--- server.js
+++ server.js
@@ -5,18 +5,33 @@ const bodyParser = require("body-parser");
 const favicon = require("serve-favicon");
 const path = require("path");
 
+const {home_routes} = require("./modules/home/home_routes.js");
+const {recipes_routes} = require("./modules/recipes/recipes_routes.js");
+
 const PORT = process.env.PORT || 3000;
 const app = express(); // Main app routes
 
 app.use(favicon(path.join(__dirname, "public", "pikachu.ico")));
-app.use(bodyParser);
 
-const {home_routes} = require("./modules/home/home_routes.js");
-const {recipes_routes} = require("./modules/recipes/recipes_routes.js");
+// parse 'content-type': 'application/json'
+app.use(bodyParser.json());
+// parse 'content-type': 'application/x-www-form-urlencoded'
+app.use(bodyParser.urlencoded());
 
 app.get("/", (req, res) => {
   res.status(200)
   .send("Root");
+});
+
+app.all("/message", (req, res) => {
+  const body = req.body;
+
+  res.status(200)
+  .send(`
+  <h1>All methods message route</h1>
+  <h3>Request query string: ${JSON.stringify(req.query)}</h3>
+  <h3>Request body: ${body && body.text}</h3>
+  `);
 });
 
 app.use("/home", home_routes); // /home
