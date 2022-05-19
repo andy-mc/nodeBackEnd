6faf297b0b7285a089cf282d9c7948a664cccb29
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-18 19:51:18 -0500

    fix bug

--- server/components/messages/messages_store.js
+++ server/components/messages/messages_store.js
@@ -3,21 +3,20 @@
 const db = require("mongoose");
 const Model = require("./messages_model");
 
-db.Promise = global.Promise;
 // TODO: try to connect to mongodb on cloud
 // mover a server
 // try to create local an stagging environment
 // connect on server.js and with dinamic enviroments
+db.Promise = global.Promise;
 db.connect("mongodb://localhost:27017/telegrom", {
   useNewUrlParser: true,
 });
 console.log("[db]: La base de datos conectada exitosamente");
 
-const list = [];
 // fix eslint
 async function addMessage(message) {
   // const db_potential_message = await Model.create(message);
-  const db_potential_message = Model.insert(message);
+  const db_potential_message = new Model(message);
   const save_message = await db_potential_message.save();
 
   return save_message;
