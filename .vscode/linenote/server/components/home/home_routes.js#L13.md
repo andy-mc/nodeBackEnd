9d10881cb3df7c6a5045f2c5c6b25df445018c0b
Andrés Muñoz <andres.munoz@idukay.com>
2022-05-21 05:53:11 -0500

    init users component

--- .vscode/linenote/server/components/home/home_routes.js#L13.md
+++
@@ -1,5 +0,0 @@
-<<<<nota mental>>>>
-
-is easier and better code to use components
-middlewares custome middlewares in
-each method when is a personalized one
--- server/network/app_router.js
+++ server/network/app_router.js
@@ -1,8 +1,7 @@
 "use strict";
 
 function app_router(app) {
-  app.use("/home", require("../components/home/home_routes"));
-  app.use("/recipes", require("../components/recipes/recipes_routes"));
+  app.use("/users", require("../components/users/users_routes"));
   app.use("/messages", require("../components/messages/messages_routes"));
   // All not found routes use not_found_handler
   app.use(require("./not_found_handler"));
